c391e9298b2a13c64e4ddf353581dfb74d244c75
Mateus Moura <chagas.mateus@gmail.com>
2019-07-01 16:33:37 -0300

    Updates

---
+++ precache-manifest.20805ed1a7e161564eba0522bd75713e.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba41aa5bca7df1fbcde95d8b7df30c2a",
+    "url": "/psdb-dash/index.html"
+  },
+  {
+    "revision": "afd344ebdf1436b87180",
+    "url": "/psdb-dash/static/css/main.ab4c6f1c.chunk.css"
+  },
+  {
+    "revision": "99d785f12e1443ba98de",
+    "url": "/psdb-dash/static/js/2.b5980e50.chunk.js"
+  },
+  {
+    "revision": "afd344ebdf1436b87180",
+    "url": "/psdb-dash/static/js/main.d9f3d0d4.chunk.js"
+  },
+  {
+    "revision": "ba05be9cc0ae6e9a37f0",
+    "url": "/psdb-dash/static/js/runtime~main.d71cb806.js"
+  },
+  {
+    "revision": "a572e46b4101bcfedaa5d21a52853866",
+    "url": "/psdb-dash/static/media/bg-countdown.a572e46b.jpg"
+  },
+  {
+    "revision": "11fc83ae11617015f2fcde2065fb34d3",
+    "url": "/psdb-dash/static/media/dripicons-v2.11fc83ae.woff"
+  },
+  {
+    "revision": "7e12564e72400735ae5b671780f6a9f7",
+    "url": "/psdb-dash/static/media/dripicons-v2.7e12564e.eot"
+  },
+  {
+    "revision": "835984438066bd8944a7692e331152a7",
+    "url": "/psdb-dash/static/media/dripicons-v2.83598443.svg"
+  },
+  {
+    "revision": "cf09c981aeaa8736810133ab1148e4a3",
+    "url": "/psdb-dash/static/media/dripicons-v2.cf09c981.ttf"
+  },
+  {
+    "revision": "0879d6e7beeb88e2c3a9beab419c5a9f",
+    "url": "/psdb-dash/static/media/feather-webfont.0879d6e7.eot"
+  },
+  {
+    "revision": "09af5c887a081a77e2f50330cb2916f5",
+    "url": "/psdb-dash/static/media/feather-webfont.09af5c88.ttf"
+  },
+  {
+    "revision": "254c5832442c4e0340e89a6f0e602c6e",
+    "url": "/psdb-dash/static/media/feather-webfont.254c5832.woff"
+  },
+  {
+    "revision": "de556ca37f103adcbcd485329ca1b909",
+    "url": "/psdb-dash/static/media/feather-webfont.de556ca3.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/psdb-dash/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/psdb-dash/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/psdb-dash/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/psdb-dash/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/psdb-dash/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "275ec7a7dfccafbe5aa3a39a7b1e6ea2",
+    "url": "/psdb-dash/static/media/logo-psdb.275ec7a7.png"
+  },
+  {
+    "revision": "6369b8c2719efb07415e7902b6e40245",
+    "url": "/psdb-dash/static/media/materialdesignicons-webfont.6369b8c2.woff"
+  },
+  {
+    "revision": "6cf92bea27d1450677d18c4856e20cb5",
+    "url": "/psdb-dash/static/media/materialdesignicons-webfont.6cf92bea.woff2"
+  },
+  {
+    "revision": "76a019b7c345101d89f49ace4f48f778",
+    "url": "/psdb-dash/static/media/materialdesignicons-webfont.76a019b7.eot"
+  },
+  {
+    "revision": "96de1bfd1952446300e0ca91f4d13247",
+    "url": "/psdb-dash/static/media/materialdesignicons-webfont.96de1bfd.ttf"
+  },
+  {
+    "revision": "a17753cdf72a4bc1b7c50e73edb85ef5",
+    "url": "/psdb-dash/static/media/materialdesignicons-webfont.a17753cd.svg"
+  }
+]);
