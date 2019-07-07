615e0917572f9bc1b653d09fe03b879ed57b3f14
Mateus Moura <chagas.mateus@gmail.com>
2019-07-07 17:58:59 -0300

    Ajustes na estrutura do Perfil

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -33,12 +33,10 @@ const Sidebar = () => (
           </li>
 
           <li>
-            <Link
-              to='/organizacao'
-            >
+            <a href="#this">
               <i className="fa fa-folder-open"></i
               ><span>Organização Partidária</span>
-            </Link>
+              <span className="menu-arrow"></span></a>
 
             <ul className="nav-second-level nav" aria-expanded="false">
               <li><Link to="organizacao/todos">Todos os diretórios</Link></li>
@@ -49,12 +47,10 @@ const Sidebar = () => (
           </li>
 
           <li>
-            <Link
-              to='/eleicoes'
-            >
+            <a href="#this">
               <i className="fa fa-gavel"></i
               ><span>Eleições</span>
-            </Link>
+              <span className="menu-arrow"></span></a>
 
             <ul className="nav-second-level nav" aria-expanded="false">
               <li><Link to="eleicoes/consulta">Consulta dados</Link></li>
@@ -63,12 +59,10 @@ const Sidebar = () => (
           </li>
 
           <li>
-            <Link
-              to='/comunicacao'
-            >
-              <i className="fa fa-gavel"></i
+            <a href="#this">
+              <i className="fa fa-envelope"></i
               ><span>Comunicação</span>
-            </Link>
+              <span className="menu-arrow"></span></a>
 
             <ul className="nav-second-level nav" aria-expanded="false">
               <li><Link to="comunicacao/consulta">Todas Redes Sociais</Link></li>
@@ -80,12 +74,10 @@ const Sidebar = () => (
           <li className="menu-title">Administrativo</li>
 
           <li>
-            <Link
-              to='/perfil'
-            >
-              <i className="fa fa-gavel"></i
+            <a href="#this">
+              <i className="fa fa-user"></i
               ><span>Perfil</span>
-            </Link>
+              <span className="menu-arrow"></span></a>
 
             <ul className="nav-second-level nav" aria-expanded="false">
               <li><Link to="perfil/consulta">Editar dados</Link></li>
@@ -93,184 +85,6 @@ const Sidebar = () => (
               <li><Link to="perfil/relatorios">Configurações</Link></li>
             </ul>
           </li>
-
-          {/* <li>
-            <a href="javascript: void(0);"
-              ><i className="fi-briefcase"></i><span>UI Kit</span>
-              <span className="menu-arrow"></span
-            ></a>
-            <ul className="nav-second-level nav" aria-expanded="false">
-              <li><a href="ui-typography.html">Typography</a></li>
-              <li><a href="ui-cards.html">Cards</a></li>
-              <li><a href="ui-buttons.html">Buttons</a></li>
-              <li><a href="ui-modals.html">Modals</a></li>
-              <li><a href="ui-checkbox-radio.html">Checkboxs-Radios</a></li>
-              <li><a href="ui-tabs.html">Tabs</a></li>
-              <li><a href="ui-progressbars.html">Progress Bars</a></li>
-              <li><a href="ui-notifications.html">Notification</a></li>
-              <li><a href="ui-carousel.html">Carousel</a></li>
-              <li><a href="ui-video.html">Video</a></li>
-              <li><a href="ui-tooltips-popovers.html">Tooltips & Popovers</a></li>
-              <li><a href="ui-images.html">Images</a></li>
-              <li><a href="ui-bootstrap.html">Bootstrap UI</a></li>
-            </ul>
-          </li>
-
-          <li>
-            <a href="javascript: void(0);"
-              ><i className="fi-box"></i><span>Icons</span
-              ><span className="menu-arrow"></span
-            ></a>
-            <ul className="nav-second-level nav" aria-expanded="false">
-              <li><a href="icons-colored.html">Colored Icons</a></li>
-              <li><a href="icons-materialdesign.html">Material Design</a></li>
-              <li><a href="icons-dripicons.html">Dripicons</a></li>
-              <li><a href="icons-fontawesome.html">Font awesome</a></li>
-              <li><a href="icons-feather.html">Feather Icons</a></li>
-            </ul>
-          </li>
-
-          <li>
-            <a href="javascript: void(0);"
-              ><i className="fi-bar-graph-2"></i><span>Graphs</span
-              ><span className="menu-arrow"></span
-            ></a>
-            <ul className="nav-second-level nav" aria-expanded="false">
-              <li><a href="chart-flot.html">Flot Chart</a></li>
-              <li><a href="chart-morris.html">Morris Chart</a></li>
-              <li><a href="chart-google.html">Google Chart</a></li>
-              <li><a href="chart-chartist.html">Chartist Charts</a></li>
-              <li><a href="chart-chartjs.html">Chartjs Chart</a></li>
-              <li><a href="chart-c3.html">C3 Chart</a></li>
-              <li><a href="chart-sparkline.html">Sparkline Chart</a></li>
-              <li><a href="chart-knob.html">Jquery Knob</a></li>
-            </ul>
-          </li>
-
-          <li>
-            <a href="javascript: void(0);"
-              ><i className="fi-disc"></i
-              ><span className="badge badge-pill badge-primary float-right">9</span
-              ><span>Forms</span></a
-            >
-            <ul className="nav-second-level nav" aria-expanded="false">
-              <li><a href="form-elements.html">Form Elements</a></li>
-              <li><a href="form-advanced.html">Form Advanced</a></li>
-              <li><a href="form-validation.html">Form Validation</a></li>
-              <li><a href="form-pickers.html">Form Pickers</a></li>
-              <li><a href="form-wizard.html">Form Wizard</a></li>
-              <li><a href="form-mask.html">Form Masks</a></li>
-              <li><a href="form-summernote.html">Summernote</a></li>
-              <li><a href="form-wysiwig.html">Wysiwig Editors</a></li>
-              <li><a href="form-uploads.html">Multiple File Upload</a></li>
-            </ul>
-          </li>
-
-          <li>
-            <a href="javascript: void(0);"
-              ><i className="fi-layout"></i><span>Tables</span
-              ><span className="menu-arrow"></span
-            ></a>
-            <ul className="nav-second-level nav" aria-expanded="false">
-              <li><a href="tables-basic.html">Basic Tables</a></li>
-              <li><a href="tables-layouts.html">Tables Layouts</a></li>
-              <li><a href="tables-datatable.html">Data Table</a></li>
-              <li><a href="tables-responsive.html">Responsive Table</a></li>
-              <li><a href="tables-tablesaw.html">Tablesaw Table</a></li>
-              <li><a href="tables-editable.html">Editable Table</a></li>
-            </ul>
-          </li>
-
-          <li className="menu-title">More</li>
-
-          <li>
-            <a href="javascript: void(0);"
-              ><i className="fi-target"></i><span>Admin UI</span
-              ><span className="menu-arrow"></span
-            ></a>
-            <ul className="nav-second-level nav" aria-expanded="false">
-              <li><a href="admin-grid.html">Grid</a></li>
-              <li><a href="admin-sweet-alert.html">Sweet Alert</a></li>
-              <li><a href="admin-widgets.html">Widgets</a></li>
-              <li><a href="admin-nestable.html">Nestable List</a></li>
-              <li><a href="admin-rangeslider.html">Range Slider</a></li>
-              <li><a href="admin-ratings.html">Ratings</a></li>
-            </ul>
-          </li>
-
-          <li>
-            <a href="calendar.html"
-              ><i className="fi-clock"></i><span>Calendar</span></a
-            >
-          </li>
-
-          <li>
-            <a href="javascript: void(0);"
-              ><i className="fi-map"></i><span>Maps</span
-              ><span className="menu-arrow"></span
-            ></a>
-            <ul className="nav-second-level nav" aria-expanded="false">
-              <li><a href="maps-google.html">Google Maps</a></li>
-              <li><a href="maps-vector.html">Vector Maps</a></li>
-              <li><a href="maps-mapael.html">Mapael Maps</a></li>
-            </ul>
-          </li>
-
-          <li className="has_sub">
-            <a href="javascript: void(0);"
-              ><i className="fi-paper-stack"></i><span>Pages</span
-              ><span className="menu-arrow"></span
-            ></a>
-            <ul className="nav-second-level nav" aria-expanded="false">
-              <li><a href="page-starter.html">Starter Page</a></li>
-              <li><a href="page-login.html">Login</a></li>
-              <li><a href="page-register.html">Register</a></li>
-              <li><a href="page-logout.html">Logout</a></li>
-              <li><a href="page-recoverpw.html">Recover Password</a></li>
-              <li><a href="page-lock-screen.html">Lock Screen</a></li>
-              <li><a href="page-confirm-mail.html">Confirm Mail</a></li>
-              <li><a href="page-404.html">Error 404</a></li>
-              <li><a href="page-404-alt.html">Error 404-alt</a></li>
-              <li><a href="page-500.html">Error 500</a></li>
-            </ul>
-          </li>
-
-          <li>
-            <a href="javascript: void(0);"
-              ><i className="fi-marquee-plus"></i><span>Extra Pages</span
-              ><span className="menu-arrow"></span
-            ></a>
-            <ul className="nav-second-level nav" aria-expanded="false">
-              <li><a href="extras-about.html">About Us</a></li>
-              <li><a href="extras-contact.html">Contact</a></li>
-              <li><a href="extras-members.html">Members</a></li>
-              <li><a href="extras-timeline.html">Timeline</a></li>
-              <li><a href="extras-invoice.html">Invoice</a></li>
-              <li><a href="extras-maintenance.html">Maintenance</a></li>
-              <li><a href="extras-coming-soon.html">Coming Soon</a></li>
-              <li><a href="extras-faq.html">FAQ</a></li>
-              <li><a href="extras-pricing.html">Pricing</a></li>
-            </ul>
-          </li>
-
-          <li>
-            <a href="javascript: void(0);" aria-expanded="true"
-              ><i className="fi-share"></i><span>Multi Level</span
-              ><span className="menu-arrow"></span
-            ></a>
-            <ul className="nav-second-level nav" aria-expanded="true">
-              <li><a href="javascript: void(0);">Level 1.1</a></li>
-              <li>
-                <a href="javascript: void(0);" aria-expanded="true"
-                  >Level 1.2<span className="menu-arrow"></span
-                ></a>
-                <ul className="nav-third-level nav" aria-expanded="true">
-                  <li><a href="javascript: void(0);">Level 2.1</a></li>
-                  <li><a href="javascript: void(0);">Level 2.2</a></li>
-                </ul>
-              </li>
-            </ul>
-          </li> */}
         </ul>
       </div>
       {/* <!-- Sidebar --> */}
