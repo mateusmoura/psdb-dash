05e95abb36e38911604f6e62934aaa451fdd5e17
Mateus Moura <chagas.mateus@gmail.com>
2019-07-07 17:38:05 -0300

    Ajustes na URL ao chamar os plugins

--- src/pages/filiados/todos/index.js
+++ src/pages/filiados/todos/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import Sidebar from '../../../components/sidebar';
-import BarraTopo from '../../../components/barra-topo';
-import ResponsiveTable from '../../../components/responsive-table';
+import React from "react";
+import Sidebar from "../../../components/sidebar";
+import BarraTopo from "../../../components/barra-topo";
+import ResponsiveTable from "../../../components/responsive-table";
 
 const FiliadosTodos = () => (
   <div id="wrapper">
@@ -19,58 +19,79 @@ const FiliadosTodos = () => (
                 <h4 className="page-title">Pedidos de filiação</h4>
                 <ol className="breadcrumb p-0 m-0">
                   <li>
-                      <a href="#this">PSDB</a>
+                    <a href="#this">PSDB</a>
                   </li>
-                  <li className="active">
-                      Dashboard
-                  </li>
+                  <li className="active">Dashboard</li>
                 </ol>
-                <div className="clearfix"></div>
+                <div className="clearfix" />
               </div>
             </div>
           </div>
 
-
           <div className="row">
             <div className="col-lg-12">
-              <div class="card">
-                <div class="card-body">
-
+              <div className="card">
+                <div className="card-body">
                   <form className="form-horizontal">
-                      <div className="form-group row">
-                          <label className="col-md-2 control-label">Text</label>
-                          <div className="col-md-10">
-                              <input type="text" className="form-control" value="Some text value..." />
-                          </div>
+                    <div className="form-group row">
+                      <label htmlFor="text" className="col-md-2 control-label">Text</label>
+                      <div className="col-md-10">
+                        <input
+                          type="text"
+                          id="text"
+                          className="form-control"
+                          // value="Some text value..."
+                        />
                       </div>
-                      <div className="form-group row">
-                          <label className="col-md-2 control-label" for="example-email">Email</label>
-                          <div className="col-md-10">
-                              <input type="email" id="example-email" name="example-email" className="form-control" placeholder="Email" />
-                          </div>
+                    </div>
+                    <div className="form-group row">
+                      <label
+                        className="col-md-2 control-label"
+                        htmlFor="example-email"
+                      >
+                        Email
+                      </label>
+                      <div className="col-md-10">
+                        <input
+                          type="email"
+                          id="example-email"
+                          name="example-email"
+                          className="form-control"
+                          placeholder="Email"
+                        />
                       </div>
-                      <div className="form-group row">
-                          <label className="col-md-2 control-label">Password</label>
-                          <div className="col-md-10">
-                              <input type="password" className="form-control" value="password" />
-                          </div>
+                    </div>
+                    <div className="form-group row">
+                      <label className="col-md-2 control-label">Password</label>
+                      <div className="col-md-10">
+                        <input
+                          type="password"
+                          className="form-control"
+                          // value="password"
+                        />
                       </div>
+                    </div>
 
-                      <div className="form-group row">
-                          <label className="col-md-2 control-label">Placeholder</label>
-                          <div className="col-md-10">
-                              <input type="text" className="form-control" placeholder="placeholder" />
-                          </div>
+                    <div className="form-group row">
+                      <label className="col-md-2 control-label">
+                        Placeholder
+                      </label>
+                      <div className="col-md-10">
+                        <input
+                          type="text"
+                          className="form-control"
+                          placeholder="placeholder"
+                        />
                       </div>
-                      <div className="form-group row">
-                          <label className="col-md-2 control-label">Text area</label>
-                          <div className="col-md-10">
-                              <textarea className="form-control" rows="5"></textarea>
-                          </div>
+                    </div>
+                    <div className="form-group row">
+                      <label className="col-md-2 control-label">
+                        Text area
+                      </label>
+                      <div className="col-md-10">
+                        <textarea className="form-control" rows="5" />
                       </div>
-
-
-
+                    </div>
                   </form>
                 </div>
               </div>
@@ -78,11 +99,10 @@ const FiliadosTodos = () => (
           </div>
 
           <div className="row">
-              <div className="col-sm-12">
-                 <ResponsiveTable titulo="Todos os Afiliados" />
-              </div>
+            <div className="col-sm-12">
+              <ResponsiveTable titulo="Todos os Afiliados" />
+            </div>
           </div>
-
         </div>
       </div>
     </div>
